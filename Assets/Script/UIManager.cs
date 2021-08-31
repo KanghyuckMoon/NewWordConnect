174c21fd14c081883beaf2a60f85ad08c3fd07f3
moon <moonkanghyuck@gmail.com>
2021-08-31 23:05:09 +0900

    20210831-6
    단어잇기UI틀 제작완료

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int panelCount = 0;
+
+    [SerializeField]
+    private GameObject scrollbar;
+
+    [SerializeField]
+    private List<Image> images;
+    [SerializeField]
+    private List<GameObject> game;
+
+    private void Start()
     {
-        
+        SetListImages();
+        SetSizeListImages();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CountPanel();
+    }
+
+    private void CountPanel()
+    {
+        panelCount = scrollbar.transform.childCount;
+
+    }
+
+    private void SetListImages()
+    {
+        for(int i = 0; i <scrollbar.transform.childCount;i++)
+        {
+            Debug.Log(scrollbar.transform.GetChild(i).transform.GetChild(0));
+            game.Add(scrollbar.transform.GetChild(i).transform.GetChild(0).gameObject);
+            images.Add(scrollbar.transform.GetChild(i).transform.GetChild(0).transform.GetComponent<Image>());
+        }
+    }
+
+    private void SetSizeListImages()
+    {
+        for(int i = 0; i < images.Count; i++)
+        {
+            if (images.Count == 1) return;
+            if (images.Count == 0) return;
+            images[i].rectTransform.sizeDelta = 
+                new Vector2(images[i].rectTransform.rect.width / (images.Count * 0.5f),
+                images[i].rectTransform.rect.height / (images.Count * 0.5f));
+
+        }
     }
 }
