6a709387333fc0430e633ac42ef52d79dd867247
moon <moonkanghyuck@gmail.com>
2021-10-07 13:33:43 +0900

    G)20211007-3

--- Assets/Script/Grapic/AreaSmoke.cs
+++ Assets/Script/Grapic/AreaSmoke.cs
@@ -10,28 +10,44 @@ public class AreaSmoke : MonoBehaviour
     private PlayerMove player;
     private bool setAreaReset = true;
     private SpriteRenderer spriteRenderer = null;
-
+    [SerializeField]
+    private bool isParticle = false;
+    [SerializeField]
+    private ParticleSystem particle;
+    //
     private void Start()
     {
         player = FindObjectOfType<PlayerMove>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         StartCoroutine(AreaSet());
+        if(isParticle)
+        {
+            particle = GetComponent<ParticleSystem>();
+        }
     }
 
     private void SetSmoke()
     {
         spriteRenderer.DOColor(new Color(0.3f, 0.3f, 0.3f, 1), 1);
+        if(isParticle)
+        particle.Play();
     }
     private void NotSetSmoke()
     {
         spriteRenderer.DOColor(new Color(1f, 1f, 1f, 0), 1);
+        if (isParticle)
+            particle.Stop();
     }
 
     private IEnumerator AreaSet()
     {
         while(true)
         {
-            if (area_index != player.nowArea)
+            if(player.nowArea == -1)
+            {
+
+            }
+            else if (area_index != player.nowArea)
             {
                 if (!setAreaReset)
                 {
