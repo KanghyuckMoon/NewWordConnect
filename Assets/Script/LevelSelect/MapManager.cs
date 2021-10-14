6c8b08573a69442640a9d05d049352d81d8d106f
moon <moonkanghyuck@gmail.com>
2021-10-15 01:44:09 +0900

    G)20211015-1

--- Assets/Script/LevelSelect/MapManager.cs
+++ Assets/Script/LevelSelect/MapManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public enum Direction
 {
@@ -13,21 +14,59 @@ public enum Direction
 
 public class MapManager : MonoBehaviour
 {
+	readonly private KeyCode[] keyCodes = {
+	KeyCode.Keypad0,
+	KeyCode.Keypad1,
+	KeyCode.Keypad2,
+	KeyCode.Keypad3,
+	KeyCode.Keypad4,
+	KeyCode.Keypad5,
+	KeyCode.Keypad6,
+	KeyCode.Keypad7,
+	KeyCode.Keypad8,
+	KeyCode.Keypad9,
+	};
+
 	public StageCharacter Character;
 	public StagePin StartPin;
 	private SaveUser saveUser;
+	private KeySetting keysetting;
+	private int optionselect = 0;
+	[SerializeField]
+	private RectTransform optionarrow = null;
+	[SerializeField]
+	private RectTransform[] optionRectTexts = null;
+	[SerializeField]
+	private Text[] optionTexts = null;
+
 	[SerializeField]
 	private Text stagenameText;
+	[SerializeField]
+	private GameObject escSystem;
+
+	private bool isEsc = false;
 
 	private void Start()
 	{
 		saveUser = SaveManager.Instance.CurrentSaveUser;
+		keysetting = SaveManager.Instance.CurrenKeySetting;
 		Character.Initialise(this, StartPin); // 캐릭터 위치 설정
 	}
 
 	private void Update()
 	{
 		if (Character.isMoving) return; // 이동 중이면 안 받음
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			isEsc = !isEsc;
+			escSystem.SetActive(isEsc);
+		}
+		if(isEsc)
+        {
+		OptionInput();
+
+        }
+		if (isEsc) return;
 
 		CheckForInput(); // 입력 받음
 	}
@@ -60,4 +99,76 @@ public class MapManager : MonoBehaviour
     {
 		stagenameText.text = name;
     }
+
+	private void OptionInput()
+	{
+		for (int i = 0; i < keyCodes.Length; i++)
+		{
+			if (Input.GetKeyDown(keyCodes[i] - (keysetting.Numpad ? 0 : 208)))
+			{
+				if (optionselect == 2) SetBackGroundVolume(i);
+				if (optionselect == 3)
+				{
+					SetEffectVolume(i);
+					SoundManager.Instance.SFXPlay(2);
+				}
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			OptionSelect(-1);
+			MoveOption();
+		}
+		else if (Input.GetKeyDown(KeyCode.S))
+		{
+			OptionSelect(1);
+			MoveOption();
+		}
+	}
+
+	private void SetTextWASD()
+	{
+		optionTexts[0].text = "WASD/방향키 - " + (keysetting.Wasd ? "WASD" : "방향키");
+	}
+
+	private void SetTextNumpad()
+	{
+		optionTexts[1].text = "넘패드/키패드 - " + (keysetting.Numpad ? "넘패드" : "키패드");
+	}
+
+	private void SetBackGroundVolume(int num)
+	{
+		SoundManager.Instance.SetBgSoundVolume(num);
+		if (num == 9)
+		{
+			optionTexts[2].text = "배경음악 - " + 100;
+		}
+		else
+		{
+			optionTexts[2].text = "배경음악 - " + (num * 10);
+		}
+	}
+	private void SetEffectVolume(int num)
+	{
+		SoundManager.Instance.SetEffectSoundVolume(num);
+		if (num == 9)
+		{
+			optionTexts[3].text = "효과음 - " + 100;
+		}
+		else
+		{
+			optionTexts[3].text = "효과음 - " + (num * 10);
+		}
+	}
+	private void MoveOption()
+	{
+		optionarrow.DOAnchorPosY(optionRectTexts[optionselect].anchoredPosition.y, 0.2f);
+	}
+
+	private void OptionSelect(int i)
+	{
+		if (optionselect + i < 0) return;
+		if (optionselect + i > optionRectTexts.Length - 1) return;
+		optionselect += i;
+	}
 }
