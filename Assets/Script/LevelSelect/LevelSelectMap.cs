35809984712091faccdf0d3a64a52daeed65c780
moon <moonkanghyuck@gmail.com>
2021-10-16 23:33:38 +0900

    G)20211016-6

--- Assets/Script/LevelSelect/LevelSelectMap.cs
+++ Assets/Script/LevelSelect/LevelSelectMap.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class LevelSelectMap : MonoBehaviour
 {
+	readonly private KeyCode[] keyCodes = {
+	KeyCode.Keypad0,
+	KeyCode.Keypad1,
+	KeyCode.Keypad2,
+	KeyCode.Keypad3,
+	KeyCode.Keypad4,
+	KeyCode.Keypad5,
+	KeyCode.Keypad6,
+	KeyCode.Keypad7,
+	KeyCode.Keypad8,
+	KeyCode.Keypad9,
+	};
+
 	public StageCharacter Character;
 	public StagePin StartPin;
 	[SerializeField]
@@ -12,11 +26,30 @@ public class LevelSelectMap : MonoBehaviour
 	[SerializeField]
 	private Text ClearText;
 	private bool isEsc;
+	private KeySetting keysetting;
+
+	//ESC 하위 오브젝트들
+	[SerializeField]
+	private GameObject EscUI;
+	private int selectedESC = 1;
+	[SerializeField]
+	private Image[] EscImages;
+	[SerializeField]
+	private Text[] EscTexts;
+	[SerializeField]
+	private Image CharacterImage;
+	[SerializeField]
+	private GameObject[] getWords;
+
+	[SerializeField]
+	private Material[] materials;
 
 	private void Start()
 	{
 		// Pass a ref and default the player Starting Pin
 		Character.Initialise(this, StartPin);
+		keysetting = SaveManager.Instance.CurrenKeySetting;
+		SetSizeImage();
 	}
 
 	private void Update()
@@ -24,7 +57,6 @@ public class LevelSelectMap : MonoBehaviour
 		// Only check input when character is stopped
 		if (Character.isMoving) return;
 		InputEsc();
-
 		if (isEsc) return;
 
 		// First thing to do is try get the player input
@@ -74,6 +106,55 @@ public class LevelSelectMap : MonoBehaviour
 		if(Input.GetKeyDown(KeyCode.Escape))
         {
 			isEsc = !isEsc;
+			EscUI.SetActive(isEsc);
+        }
+		else
+		{
+			for (int i = 0; i < keyCodes.Length; i++)
+			{
+				if (Input.GetKeyDown(keyCodes[i] - (keysetting.Numpad ? 0 : 208)))
+				{
+					if (i < 3 && i > 0)
+					{
+						selectedESC = i;
+						SetSizeImage();
+					}
+				}
+			}
+		}
+	}
+	private void SetSizeImage()
+    {
+		for(int i = 1; i<EscImages.Length;i++)
+        {
+			if(i == selectedESC)
+			{
+				EscImages[i].GetComponent<RectTransform>().DOSizeDelta(new Vector2(420,80), 0.3f);
+				EscTexts[i].GetComponent<RectTransform>().DOScale(new Vector2(1,1), 0.3f);
+				EscImages[i].material = materials[1];
+				if (EscImages[i].GetComponent<RectTransform>().anchoredPosition.x > 0)
+				{
+					EscImages[i].GetComponent<RectTransform>().DOAnchorPosX(140, 0.3f);
+				}
+				else
+				{
+					EscImages[i].GetComponent<RectTransform>().DOAnchorPosX(-140, 0.3f);
+                }
+			}
+			else
+			{
+				EscImages[i].GetComponent<RectTransform>().DOSizeDelta(new Vector2(340, 50), 0.3f);
+				EscTexts[i].GetComponent<RectTransform>().DOScale(new Vector2(0.7f, 0.7f), 0.3f);
+				EscImages[i].material = materials[0];
+				if (EscImages[i].GetComponent<RectTransform>().anchoredPosition.x > 0)
+				{
+					EscImages[i].GetComponent<RectTransform>().DOAnchorPosX(177, 0.3f);
+				}
+				else
+				{
+					EscImages[i].GetComponent<RectTransform>().DOAnchorPosX(-177, 0.3f);
+				}
+			}
         }
     }
 }
