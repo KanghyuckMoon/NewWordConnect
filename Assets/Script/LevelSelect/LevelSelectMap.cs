ab958ccc5d214817031b225d57e7f599dc989193
moon <moonkanghyuck@gmail.com>
2021-10-18 12:54:21 +0900

    G)20211018-10

--- Assets/Script/LevelSelect/LevelSelectMap.cs
+++ Assets/Script/LevelSelect/LevelSelectMap.cs
@@ -50,26 +50,9 @@ public class LevelSelectMap : MonoBehaviour
 	private RectTransform[] getconditionWords;
 	[SerializeField]
 	private RectTransform[] getexecutionWords;
-	//옵션 패널
-	[SerializeField]
-	private GameObject optionTextObj;
-	[SerializeField]
-	private RectTransform[] optionTexts;
-	[SerializeField]
-	private GameObject keysettingObj;
-	[SerializeField]
-	private GameObject[] keysettingBtn;
-	[SerializeField]
-	private RectTransform[] keysettingoptionTexts;
-	[SerializeField]
-	private GameObject soundsettingObj;
-	[SerializeField]
-	private RectTransform[] soundsettingoptionTexts;
-	[SerializeField]
-	private RectTransform selectImage;
-	private int optionselect = 0;
-	private int nowoptionselect = -1;
 	private bool isKeySetting;
+
+	private OptionPanel optionPanel;
 
 	[SerializeField]
 	private Material[] materials;
@@ -80,10 +63,9 @@ public class LevelSelectMap : MonoBehaviour
 		Character.Initialise(this, StartPin);
 		keysetting = SaveManager.Instance.CurrenKeySetting;
 		saveuser = SaveManager.Instance.CurrentSaveUser;
+		optionPanel = GetComponent<OptionPanel>();
 		SetSizeImage();
 		SetActiveWords();
-		OptionObjSetActive();
-		WASDKeySetting(keysetting.Wasd);
 	}
 
 	private void Update()
@@ -145,133 +127,24 @@ public class LevelSelectMap : MonoBehaviour
 			backgroundObj[0].SetActive(!isEsc);
 			backgroundObj[1].SetActive(!isEsc);
         }
-		else if(selectedESC == 2 && isEsc)
-        {
-			if(Input.GetKeyDown((KeyCode)keysetting.wasdKeyCodes[0])) // W
-            {
-				optionselect--;
-				if (optionselect < 0)
-                {
-					optionselect = 0;
-					return;
-				}
-				MoveOptionSelect();
-			}
-			else if(Input.GetKeyDown((KeyCode)keysetting.wasdKeyCodes[1])) // S
-			{
-				optionselect++;
-				if(nowoptionselect == -1)
-				{
-					if (optionselect > optionTexts.Length - 1)
-					{
-						optionselect = optionTexts.Length - 1;
-						return;
-					}
-				}
-				else if(nowoptionselect == 0)
-                {
-					if (optionselect > keysettingoptionTexts.Length - 1)
-					{
-						optionselect = keysettingoptionTexts.Length - 1;
-						return;
-					}
-				}
-				else if (nowoptionselect == 1)
-				{
-					if (optionselect > soundsettingoptionTexts.Length - 1)
-					{
-						optionselect = soundsettingoptionTexts.Length - 1;
-						return;
-					}
-				}
-				MoveOptionSelect();
-			}
-			else if (Input.GetKeyDown(KeyCode.Space))
-			{
-				if (nowoptionselect == -1)
-                {
-					nowoptionselect = optionselect;
-					optionselect = 0;
-					OptionObjSetActive();
-					MoveOptionSelect();
-				}
-				else if(nowoptionselect == 0)
-				{
-					OptionObjSetActive();
-					MoveOptionSelect();
-					if(optionselect == 0)
-                    {
-						keysettingBtn[0].SetActive(true);
-						keysettingBtn[1].SetActive(false);
-						isKeySetting = true;
-                    }
-					else if(optionselect == 1)
-					{
-						keysettingBtn[0].SetActive(false);
-						keysettingBtn[1].SetActive(true);
-						isKeySetting = true;
-					}
-					optionselect = 0;
-				}
-				else if(nowoptionselect == 1)
-				{
-
-				}
-				else if(nowoptionselect == 2)
-                {
-					SceneManager.LoadScene("MainTitle");
-                }
-				else if(nowoptionselect == 3)
-				{
-					#if UNITY_EDITOR
-					UnityEditor.EditorApplication.isPlaying = false;
-					#else
-                            Application.Quit(); // 어플리케이션 종료
-					#endif
-				}
-			}
-			else if (Input.GetKeyDown(KeyCode.Backspace))
-			{
-				nowoptionselect = -1;
-				optionselect = 0;
-				OptionObjSetActive();
-				MoveOptionSelect();
-			}
-			else
-            {
-				for (int i = 0; i < keyCodes.Length; i++)
-				{
-					if (Input.GetKeyDown(keyCodes[i] - (keysetting.Numpad ? 0 : 208)))
-					{
-						if(nowoptionselect == 1)
-                        {
-							if(optionselect == 0)
-                            {
-								SetBackGroundVolume(i);
-                            }
-							else
-                            {
-								SetEffectVolume(i);
-                            }
-                        }
-						else if (i < 3 && i > 0)
-						{
-							selectedESC = i;
-							SetSizeImage();
-						}
-					}
-				}
-			}
-        }
 		else
 		{
 			for (int i = 0; i < keyCodes.Length; i++)
 			{
 				if (Input.GetKeyDown(keyCodes[i] - (keysetting.Numpad ? 0 : 208)))
 				{
-						if (i < 3 && i > 0)
+					if (optionPanel.ReturnToisDontNum()) return;
+					if (i < 3 && i > 0)
 						{
 							selectedESC = i;
+						if (i == 2)
+						{
+							optionPanel.isEnabled = true;
+						}
+						else
+						{
+							optionPanel.isEnabled = false;
+						}
 							SetSizeImage();
 						}
 				}
@@ -361,125 +234,5 @@ public class LevelSelectMap : MonoBehaviour
 		}
 
 		Invoke("ShakeWords", 5);
-	}
-
-	private void MoveOptionSelect()
-    {
-		if(nowoptionselect == -1)
-        {
-			selectImage.DOAnchorPosY(optionTexts[optionselect].anchoredPosition.y, 0.2f);
-			switch (optionselect)
-			{
-				case 1:
-					selectImage.DOAnchorPosX(-130, 0.2f);
-					break;
-				case 2:
-					selectImage.DOAnchorPosX(-220, 0.2f);
-					break;
-				default:
-					selectImage.DOAnchorPosX(-116, 0.2f);
-					break;
-			}
-		}
-		else if (nowoptionselect == 0)
-		{
-			selectImage.DOAnchorPosY(keysettingoptionTexts[optionselect].anchoredPosition.y, 0.2f);
-			selectImage.DOAnchorPosX(-220, 0.2f);
-		}
-		else if (nowoptionselect == 1)
-		{
-			selectImage.DOAnchorPosY(soundsettingoptionTexts[optionselect].anchoredPosition.y, 0.2f);
-			selectImage.DOAnchorPosX(-140, 0.2f);
-		}
-		
-    }
-
-	private void OptionObjSetActive()
-    {
-		if(nowoptionselect == -1)
-        {
-			keysettingObj.SetActive(false);
-			keysettingBtn[0].SetActive(false);
-			keysettingBtn[1].SetActive(false);
-			soundsettingObj.SetActive(false);
-			optionTextObj.SetActive(true);
-        }
-		else if (nowoptionselect == 0)
-		{
-			keysettingObj.SetActive(true);
-			keysettingBtn[0].SetActive(false);
-			keysettingBtn[1].SetActive(false);
-			soundsettingObj.SetActive(false);
-			optionTextObj.SetActive(false);
-		}
-		else if (nowoptionselect == 1)
-		{
-			keysettingObj.SetActive(false);
-			keysettingBtn[0].SetActive(false);
-			keysettingBtn[1].SetActive(false);
-			soundsettingObj.SetActive(true);
-			optionTextObj.SetActive(false);
-		}
-	}
-
-	public void SetWASD(bool input)
-	{
-		keysetting.Wasd = input;
-		SaveManager.Instance.SaveKeySetting();
-		isKeySetting = false;
-		WASDKeySetting(input);
-		keysettingBtn[0].SetActive(false);
-		keysettingBtn[1].SetActive(false);
-	}
-	public void SetKeyPad(bool input)
-	{
-		keysetting.Numpad = input;
-		SaveManager.Instance.SaveKeySetting();
-		isKeySetting = false;
-		keysettingBtn[0].SetActive(false);
-		keysettingBtn[1].SetActive(false);
-	}
-
-	private void WASDKeySetting(bool input)
-    {
-		if (input)
-		{
-			keysetting.wasdKeyCodes[0] = (int)KeyCode.W;
-			keysetting.wasdKeyCodes[1] = (int)KeyCode.S;
-			keysetting.wasdKeyCodes[2] = (int)KeyCode.D;
-			keysetting.wasdKeyCodes[3] = (int)KeyCode.A;
-		}
-		else
-		{
-			keysetting.wasdKeyCodes[0] = (int)KeyCode.UpArrow;
-			keysetting.wasdKeyCodes[1] = (int)KeyCode.DownArrow;
-			keysetting.wasdKeyCodes[2] = (int)KeyCode.LeftArrow;
-			keysetting.wasdKeyCodes[3] = (int)KeyCode.RightArrow;
-		}
-	}
-
-	private void SetBackGroundVolume(int num)
-	{
-		SoundManager.Instance.SetBgSoundVolume(num);
-		if (num == 9)
-		{
-			soundsettingoptionTexts[0].GetComponent<Text>().text = "배경음악 - " + 100;
-		}
-		else
-		{
-			soundsettingoptionTexts[0].GetComponent<Text>().text = "배경음악 - " + (num * 10);
-		}
-	}
-	private void SetEffectVolume(int num)
-	{
-		SoundManager.Instance.SetEffectSoundVolume(num);
-		if (num == 9)
-		{
-			soundsettingoptionTexts[1].GetComponent<Text>().text = "효과음 - " + 100;
-		}
-		else
-		{
-			soundsettingoptionTexts[1].GetComponent<Text>().text = "효과음 - " + (num * 10);
-		}
 	}
 }
